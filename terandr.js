e1cd632aa46e9455ac4429fe8c9e1fd0abd8323a
waterandroid <waterandroid@users.noreply.github.com>
2015-04-18 00:48:42 -0400

    Foundation Work
    -Some reformatting of variables and whitespace
    -Added variables to act as constants
    -Changed Command Intepreter to Switch Case instead of many else ifs.
    -Start work on Accusing Players
    -Can confirm if a player is on a given side

--- terandr.js
+++ terandr.js
@@ -1,83 +1,110 @@
 var IRC = require("internet-relay-chat");
+//START EDITS HERE//
 var bot = new IRC({
 	"username": "MafiaBot",
-	"nick": "MafiaBot",
+	"nick": "MafiaBot"
 });
 var mainChannel = "";
 var admin = "";
 var specialChar = "-";
-
+//STOP EDITING//
 
 var players = {
 	mafia: [
-		{name: 		"apple",
+		{nick: 			"apple",
 		godfather: 	false},
-		{name: 		"banana",
+		{nick: 			"banana",
 		godfather: 	true}
 	],
 	innocent: [
-		{name:		"carrot",
+		{nick:			"carrot",
 		detective:	true,
-		angel:		false},
-		{name:		"dount",
+		angel:			false},
+		{nick:			"dount",
 		detective:	false,
-		angel:		true},
-		{name:		"elephant",
+		angel:			true},
+		{nick:			"elephant",
 		detective:	false,
-		angel:		false},
-		{name:		"freedom",
+		angel:			false},
+		{nick:			"freedom",
 		detective:	false,
-		angel:		false},
-		{name:		"ginger",
+		angel:			false},
+		{nick:			"ginger",
 		detective:	false,
-		angel:		false}
+		angel:			false}
 	],
 	dead: [
-		{name: 		"honey",
-		mafia: 		true},
-		{name: 		"infared",
-		mafia: 		false}
+		{nick: 			"honey",
+		mafia: 			true},
+		{nick: 			"infared",
+		mafia: 			false}
 	]
 };
+var mafia = players.mafia;
+var innocent = players.innocent;
+var dead = players.dead;
 
 //Start of IRC Server Connection Stuff
 bot.on("connect", function() {
-    console.log("Bot connected");
+  console.log("Bot connected");
 });
 
 bot.connect();
 
 bot.on("registered", function() {
-    bot.join(mainChannel);
+  bot.join(mainChannel);
 	console.log("Bot registered");
 });
 
 bot.on("join", function(user, channel) {
-	if(user.nick == this.options.nick) console.log("Bot joined " + channel);
+	if (user.nick == this.options.nick) console.log("Bot joined " + channel);
 });
 //End of IRC Server Connection Stuff
 
 bot.on("message", function(sender, channel, message) {
-   if(sender.hostmask == admin) console.log("Sent From Admin");
-	if(message.charAt(0) == specialChar) {
-		if(message.indexOf("repeat") == 1) {
-			repeatMessage(message.split(" ", 2));
-		} else if(message.indexOf("accuse") == 1) {
-			accusePlayer(message.split(" ", 2));
+	var commandAccess;
+	var adminCommand = false;
+  if (sender.hostmask == admin) {
+		console.log("Sent From Admin");
+		adminCommand = true;
+	}
+
+	if (message.charAt(0) == specialChar) {
+		var command = message.split(" ")[0];
+		switch (command.substring(1)) {
+			case "repeat":
+				if (adminCommand) {
+					repeatMessage(message.split(" ", 2)[1]);
+				}
+				break;
+			case "accuse":
+				if (adminCommand || commandAccess == "detective") {
+					accusePlayer(message.split(" ", 2)[1]);
+				}
+				break;
+			default:
+				bot.message(mainChannel, "Unknown command");
 		}
 	}
 });
 
-
 function repeatMessage(message) {
-	console.log("Sending message to " + mainChannel + ": " + message[1]);
-	bot.message(mainChannel, message[1]);
+	console.log("Sending message to " + mainChannel + ": " + message);
+	bot.message(mainChannel, message);
 }
 
 function accusePlayer(message) {
-
+	console.log("Accusing player: " + message);
+	if (confirmPlayerName(message, mafia)) {
+		console.log(message + " Found");
+	}
 }
 
-function confirmPlayerName(name, side, role) {
-
+function confirmPlayerName(nickCheck, side, role) {
+	for (i = 0; i < side.length; i++) {
+		console.log(side[i].nick);
+		if (side[i].nick == nickCheck) {
+			return true;
+		}
+	}
 }
